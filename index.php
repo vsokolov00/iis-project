1cfdfc450be692fe03b74f95a29897bd964c7859
Matej Gotzman <xgotzm00@vutbr.cz>
2021-10-22 21:15:10 +0200

    Updated navbar, splitted to header and footer for easier including to different views.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include "./header.html";
+include "./navbar.html";
+//include "./created-auctions-list.php";
+//include "./user-list.php";
+//include "./auction-detail.php";
+include "./footer.html";
+?>
