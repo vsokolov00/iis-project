5d4f5d091a6fd461cfe3bbbbbf4b15e2526b1723
Šimon Strýček <xstryc06@vutbr.cz>
2021-11-06 21:04:54 +0100

    Fixed login page layout.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,66 +3,59 @@
 @section('content')
 <div class="container page">
     <div class="jumbotron-fluid">
-            <h1 id="login-header">Přihlášení</h1>
 
-            <form method="POST" action="{{ route('login') }}" id="loginForm" name="loginForm">
-                @csrf
+        <h1 id="login-header">Přihlášení</h1>
 
-                <div class="input-group col-xs-4">
-                    <div class="login">
-                        <input id="login-username" type="email" class="form-control @error('email') is-invalid @enderror text-input" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Uživatelské jméno">
-                            <i class="material-icons form-icon">person</i>
-                        </input>
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                        <b class="err-label" id="invalid-username"></b>
-                    </div>
+        <form method="POST" id="loginForm" name="loginForm">
+
+            <div class="input-group col-xs-4">
+                <div class="login">
+                    <input id="login-username" type="email" class="form-control @error('email') is-invalid @enderror text-input" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Uživatelské jméno">
+                        <i class="material-icons form-icon">person</i>
+                    </input>
                 </div>
+                <b class="err-label" id="invalid-username">
+                    @error('email')
+                        {{ $message }}
+                    @enderror
+                </b>
+            </div>
 
-                <div class="input-group col-xs-4">
-                    <div class="password">
-                        <input id="login-password" type="password" class="form-control @error('password') is-invalid @enderror text-input" name="password" required autocomplete="current-password" placeholder="Heslo">
-                            <a href="#" class="form-icon"><i id="eyeIcon" class="material-icons">visibility_off</i></a>
-                        </input>
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                        <b class="err-label" id="invalid-password"></b>
-                    </div>
+            <div class="input-group col-xs-4">
+                <div class="login">
+                    <input id="login-password" type="password" class="form-control @error('password') is-invalid @enderror text-input" name="password" required autocomplete="current-password" placeholder="Heslo">
+                        <a href="#" class="form-icon"><i id="eyeIcon" class="material-icons">visibility_off</i></a>
+                    </input>
                 </div>
-                <!--
-                <div class="input-group col-xs-4">
-                    <div class="col-md-6 offset-md-4">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                <b class="err-label" id="invalid-password">
+                    @error('password')
+                        {{ $message }}
+                    @enderror
+                </b>
+            </div>
 
-                            <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                -->
-
-                <div class="input-group col-xs-3">
-                    <button type="submit" class="btn btn-primary button-yellow btn-group-lg btn-block">
-                        Přihlásit
-                    </button>
-                    <!--
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
-                    -->
-                </div>
-            </form>
-        </div>
+            <div class="input-group col-xs-3">
+                <button type="submit" class="btn btn-primary button-yellow btn-group-lg btn-block">Přihlásit</button>
+            </div>
+        </form>
     </div>
 </div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        $("#eyeIcon").click(function (event) {
+            if($("#password").attr("type") == "password")
+            {
+                $("#password").attr("type", "text");
+                $("#eyeIcon").text("visibility");
+            }
+            else
+            {
+                $("#password").attr("type", "password");
+                $("#eyeIcon").text("visibility_off");
+            }
+        });
+    });
+</script>
 @endsection
