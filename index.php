4b939fa38d19e09b18effba77ff815c08945eac0
Vladislav <sokolovskii.vladislav@gmail.com>
2021-11-11 10:33:58 +0100

    Deleted unnecessary files

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "./header.html";
-include "./navbar.html";
-//include "./created-auctions-list.php";
-//include "./user-list.php";
-//include "./auction-detail.php";
-include "./footer.html";
-?>
