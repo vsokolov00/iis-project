e6175023cb2bb2b3627c1b9567a337443dd3c958
Šimon Strýček <xstryc06@vutbr.cz>
2021-11-14 23:55:22 +0100

    Implemented username and email editability.

--- app/Http/Controllers/EditUserController.php
+++ app/Http/Controllers/EditUserController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
 
 class EditUserController extends Controller
 {
@@ -11,5 +14,19 @@ class EditUserController extends Controller
     public function index()
     {
         return view('user/edit');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        if(isset($request->username))
+            User::where('id', Auth::user()->id)->update(['name' => $request->username]);
+        else if(isset($request->email))
+            User::where('id', Auth::user()->id)->update(['email' => $request->email]);
+        else if(isset($request->new_password) && isset($request->old_password) && isset($request->password_confirm))
+            return "Password";
+        else
+            return "None";
+
+        return redirect('profile');
     }
 }
