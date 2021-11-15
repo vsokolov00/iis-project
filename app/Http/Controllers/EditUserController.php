bf6b2bb11f5b9590f58b3080dc0fb4d6b8be08de
Šimon Strýček <xstryc06@vutbr.cz>
2021-11-15 20:32:02 +0100

    Added editing functionality.

--- app/Http/Controllers/EditUserController.php
+++ app/Http/Controllers/EditUserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
@@ -23,9 +24,20 @@ class EditUserController extends Controller
         else if(isset($request->email))
             User::where('id', Auth::user()->id)->update(['email' => $request->email]);
         else if(isset($request->new_password) && isset($request->old_password) && isset($request->password_confirm))
-            return "Password";
-        else
-            return "None";
+        {
+            if(Hash::check($request->old_password, auth()->user()->password))
+            {
+                if($request->new_password == $request->password_confirm)
+                {
+                    User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
+                    return view('user/edit')->with(['password' => 'changed']);
+                }
+                else
+                    return view('user/edit')->with(['password' => 'nonmatching']);
+            }
+            else
+                return view('user/edit')->with(['password' => 'invalid']);
+        }
 
         return redirect('profile');
     }
