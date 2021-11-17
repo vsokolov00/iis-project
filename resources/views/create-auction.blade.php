ea0df294263b5a200609aebc74104281eeb5e94f
Matej Gotzman <xgotzm00@vutbr.cz>
2021-11-17 20:06:26 +0100

    Fixed create auction.

--- resources/views/create-auction.blade.php
+++ resources/views/create-auction.blade.php
@@ -4,32 +4,41 @@
   <div class="container">
     <form action="create_auction" method="POST" files="true" enctype="multipart/form-data">
       @csrf
-      <div class="d-sm-flex  flex-md-row my-1">
-        <div class="col">
+      <div class="d-md-flex align-items-center flex-lg-row my-1">
+      <div class="col">
+        <div class="d-flex justify-content-center align-items-end ">
+          <img id="previewImg" src="../../img/empty.png" class="previewImg img-fluid"/>
+          
+          <label for="image" class="addImgBtn">
+          <span class="material-icons-outlined md-36">add</span>
+          </label>
+          <input class="d-none" type="file" accept="image/*" name="image" id="image" onchange="showPreview(this)">
+        </div> 
+      </div>
+      <div class="col">
           <label for="name">Název</label>
           <input type="text" class="form-control" id="name" name="name" required oninvalid="this.setCustomValidity('Vyplnite toto policko')">
-        </div>
+
+          <label for="name">Popis</label>
+          <textarea class="form-control" id="description" name="description" rows="4" required oninvalid="this.setCustomValidity('Vyplnite toto policko')"></textarea>
+
+      </div>
+      </div>
+      <div class="d-md-flex  flex-lg-row my-1">
         <div class="col">
           <label for="startPrice">Počáteční cena</label>
           <input type="text" class="form-control" id="stratPrice" name="stratPrice" required oninvalid="this.setCustomValidity('Vyplnite toto policko')">
         </div>
-      </div>
-
-      <div class="d-sm-flex  flex-md-row my-1">
         <div class="col">
-          <label for="name">Popis</label>
-          <input type="text" class="form-control" id="description" name="description" required oninvalid="this.setCustomValidity('Vyplnite toto policko')">
+          <label for="bidRange">Rozsah příhozů</label>
+          <div class="d-flex" id="bidRange">
+            <input type="number" class="form-control mr-1" id="stratRange">
+            <input type="number" class="form-control" id="endRange">
+          </div>
         </div>
       </div>
 
-      <div class="d-sm-flex  flex-md-row my-1">
-        <div class="col">
-          <label for="closingPrice">Uzaviraci cena</label>
-          <input type="text" class="form-control" id="closingPrice" name="closingPrice">
-        </div>
-      </div>
-
-      <div class="d-sm-flex  flex-sm-row my-1">
+      <div class="d-md-flex  flex-lg-row my-1">
         <div class="col">
           <label for="auctionStart">Začátek aukce</label>
           <input type="datetime-local" class="form-control" id="auctionStart" name="auctionStart" required oninvalid="this.setCustomValidity('Vyplnite toto policko')"/>
@@ -52,46 +61,37 @@
             })
           </script>
       </div>
-      <div class="d-sm-flex  flex-sm-row my-1">
+      <div class="d-md-flex  flex-lg-row my-1">
         <div class="col">
           <label for="type">Typ aukce</label>
           <div class="d-flex choose-from-2" id="type">
-            <div class="col chooseBtn label-green text-center chooseBtn-not-checked" id="buy" onclick="chooseFromClicked('#buy', '#sell')">Nákup</div>
-            <div class="col chooseBtn label-yellow text-center" id="sell" onclick="chooseFromClicked('#sell', '#buy')">Prodej</div>
-            <!--EASIER FOR THE MODEL, SOLVE LATER-->
-            <input type="radio" name="is_selling" id="buy" value="0" />
-            <label for="buy">Nakup</label>
-            <input type="radio" checked="checked" name="is_selling" id="sell" value="1" />
-            <label for="sell">Prodej</label>
-            <!---->
+            <div class="col chooseBtn label-green text-center chooseBtn-not-checked" id="btnbuy" onclick="chooseFromClicked('#btnbuy', '#btnsell')">
+              Nákup
+              <input type="radio" name="is_selling" id="buy" class="d-none" value="0" />
+            </div>
+            <div class="col chooseBtn label-yellow text-center" id="btnsell" onclick="chooseFromClicked('#btnsell', '#btnbuy')">
+              Prodej
+              <input type="radio" checked="checked" name="is_selling" id="sell" class="d-none" value="1" />
+            </div>
           </div>
         </div>
         <div class="col">
           <label for="rules">Pravidla aukce</label>
           <div class="d-flex choose-from-2" id="rules">
-            <div class="col chooseBtn label-green text-center " id="open" onclick="chooseFromClicked('#open', '#closed')">Otevřená</div>
-            <div class="col chooseBtn label-yellow text-center chooseBtn-not-checked" id="closed" onclick="chooseFromClicked('#closed', '#open')">Uzavřená</div>
-            <!--EASIER FOR THE MODEL, SOLVE LATER-->
-            <input type="radio" checked="checked" name="is_open" id="open" value="1" />
-            <label for="open">Otevřená</label>
-            <input type="radio" name="is_open" id="closed" value="0" />
-            <label for="closed">Uzavřená</label>
-            <!---->
+            <div class="col chooseBtn label-green text-center " id="btnopen" onclick="chooseFromClicked('#btnopen', '#btnclosed')">
+              Otevřená
+              <input type="radio" checked="checked" name="is_open" id="open" class="d-none" value="1" />
+            </div>
+            <div class="col chooseBtn label-yellow text-center chooseBtn-not-checked" id="btnclosed" onclick="chooseFromClicked('#btnclosed', '#btnopen')">
+              Uzavřená
+              <input type="radio" name="is_open" id="closed" class="d-none" value="0" />
+            </div>
           </div>
         </div>
+        </div>      
         <div class="col">
-          <label for="bidRange">Rozsah příhozů</label>
-          <div class="d-flex" id="bidRange">
-            <input type="number" class="form-control mr-1" id="stratRange">
-            <input type="number" class="form-control" id="endRange">
-          </div>
+          <input type="submit" value="Odeslat ke schválení" class="btn btn-success btn-block mt-2">
         </div>
-
-        <!--TODO styles!!!-->
-        <input type="file" accept="image/*" name="image">
-
-        <input type="submit" value="Vytvorit">
-
-      </div>
+      
   </div>
 @endsection
