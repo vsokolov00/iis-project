0df52763035ac572e1519e13d3781ea568d608fc
Šimon Strýček <xstryc06@vutbr.cz>
2021-11-27 16:56:02 +0100

    Fixed invalid auctions categorization.

--- app/Http/Controllers/AllAuctionsController.php
+++ app/Http/Controllers/AllAuctionsController.php
@@ -21,6 +21,8 @@ class AllAuctionsController extends Controller
 
     private function getAllBids($auctions)
     {
+        $bids = [];
+
         foreach ($auctions as $auction) {
             $bids[$auction->id] = Auction::find($auction->id)->participants->sum('last_bid');
         }
@@ -31,7 +33,7 @@ class AllAuctionsController extends Controller
     public function sellingAuctions()
     {
         $data = Auction::with('auctionItem')->where('is_selling', '=', '1')->where('is_approved', '=', '1')
-            ->where('time_limit', '<', now())->orderBy('start_time')->get();
+            ->where('time_limit', '>', now())->orderBy('start_time')->get();
 
         App::setLocale('cs');
         return view('allAuctions', ["auctions" => $data, "bids" => $this->getAllBids($data), "title" => "Lidé prodávají"]);
@@ -40,7 +42,7 @@ class AllAuctionsController extends Controller
     public function buyingAuctions()
     {
         $data = Auction::with('auctionItem')->where('is_selling', '=', '0')->where('is_approved', '=', '1')
-            ->where('time_limit', '<', now())->orderBy('start_time')->get();
+            ->where('time_limit', '>', now())->orderBy('start_time')->get();
 
         App::setLocale('cs');
         return view('allAuctions', ["auctions" => $data, "bids" => $this->getAllBids($data), "title" => "Lidé shánějí"]);
@@ -48,7 +50,7 @@ class AllAuctionsController extends Controller
 
     public function closestAuctions()
     {
-        $data = Auction::with('auctionItem')->orderBy('start_time')->where('start_time', '<', now())->where('is_approved', '=', '1')->get();
+        $data = Auction::with('auctionItem')->orderBy('start_time')->where('start_time', '>', now())->where('is_approved', '=', '1')->get();
 
         App::setLocale('cs');
         return view('allAuctions', ["auctions" => $data, "bids" => $this->getAllBids($data), "title" => "Nejbližší aukce"]);
