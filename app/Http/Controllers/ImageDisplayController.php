a328003a96dde4d8a61a4f37202c26b6afb0ecbd
Vladislav <sokolovskii.vladislav@gmail.com>
2021-11-27 22:31:46 +0100

    Images are cached on the client side

--- app/Http/Controllers/ImageDisplayController.php
+++ app/Http/Controllers/ImageDisplayController.php
@@ -11,7 +11,13 @@ use Storage;
 class ImageDisplayController extends Controller
 {
     public function displayImage($filename) {
+        if (Storage::disk('local')->has("public/images/" . $filename)) {
+            return  Storage::disk('local')->response("public/images/" . $filename);
+        }
         if (Storage::disk('s3')->has("public/images/" . $filename)) {
+            $s3_file = Storage::disk('s3')->get("public/images/" . $filename);
+            $local_storage = Storage::disk('local');
+            $local_storage->put("public/images/" . $filename, $s3_file);
             return  Storage::disk('s3')->response("public/images/" . $filename);
         } else {
             return  Storage::disk('s3')->response("public/images/empty.png");
