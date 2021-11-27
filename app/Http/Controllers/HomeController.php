df432cf454a684029e4273f1d5ecab1146edd324
Šimon Strýček <xstryc06@vutbr.cz>
2021-11-27 10:18:06 +0100

    Changed condition to show only active auctions. (Not ended ones.)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,13 +32,13 @@ class HomeController extends Controller
             $bids[$auction->id] = Auction::find($auction->id)->participants->sum('last_bid');
         }
 
-        $endSoon = Auction::with('auctionItem')->where('is_approved', 1)->where('start_time', '<', date('Y-m-d H:i:s'))->
-        where('time_limit', '>', date('Y-m-d H:i:s'))->orderBy('time_limit')->take(7)->get();
+        $endSoon = Auction::with('auctionItem')->where('is_approved', 1)->where('start_time', '<', now())->
+            where('time_limit', '>', now())->orderBy('time_limit')->take(7)->get();
 
         foreach ($endSoon as $auction) {
             $bids[$auction->id] = Auction::find($auction->id)->participants->sum('last_bid');
         }
-        
+
         return view('home', ["auctions" => $data, "endSoon" => $endSoon, "bids" => $bids]);
     }
 }
