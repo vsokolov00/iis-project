6bedcaa6924d2144b6b5ac104bd5da91ad35b892
Matej Gotzman <xgotzm00@vutbr.cz>
2021-11-28 11:04:39 +0100

    Added final price to created auctions view, added edit button to mobile view

--- resources/views/user/userAuctions.blade.php
+++ resources/views/user/userAuctions.blade.php
@@ -50,13 +50,13 @@
                             <td class="text-right">
                                 {{number_format($auction->starting_price,0,"", " ")}} Kč
                             </td>
-                            @if($auction->closing_price == null)
-                                <td class="text-center">-</td>
-                            @else
+                            @isset($actualPrices[$auction->id])
                                 <td class="text-right">
-                                        {{number_format($auction->closing_price ,0 ,"", " ")}}	Kč
+                                        {{number_format($actualPrices[$auction->id] ,0 ,"", " ")}}	Kč
                                 </td>
-                            @endif
+                            @else    
+                                <td class="text-right">-</td>   
+                            @endisset
                             <td class="text-right align-middle" style="padding-right:0">
                             <span class="clickable material-icons-outlined green-text md-24 pl-4 pr-3 text-right dont-propagate clickable" style="height: 24px;" onclick="openModal('{{ $auction->id }}',
                                     '{{ route('image.displayImage',$auction->auctionItem->image) }}', '{{ $auction->auctionItem->item_name }}', `{{ $auction->auctionItem->description }}`,
@@ -72,9 +72,9 @@
 				<tbody>
 					@foreach ($auctions as $auction)
                         <tr>
-                            <td onclick="openModal('{{ $auction->id }}', '{{ route('image.displayImage',$auction->auctionItem->image) }}', '{{ $auction->auctionItem->item_name }}', `{{ $auction->auctionItem->description }}`,
-                                '{{ $auction->starting_price }}', '{{ $auction->closing_price }}', '{{ $auction->is_approved }}', '{{ $auction->bid_min }}', '{{ $auction->bid_max }}', '{{ $auction->start_time }}', '{{ $auction->time_limit }}', '{{ $auction->is_open  }}', '{{ $auction->is_selling }}')">
-                                <table class="table-plain">
+                            <?php $location = route('auctionDetail',  $auction->id); ?>
+                            <td onclick="window.location='{{$location}}'">
+                                <table class="table-plain" style="width:100%;">
                                     <tbody>
                                         <tr>
                                         <th scope="row">Název</th>
@@ -114,13 +114,26 @@
                                         </tr>
                                         <tr>
                                             <th scope="row">Konečná cena</th>
-                                            @if($auction->closing_price == null)
-                                                <td class="text-center">-</td>
-                                            @else
+                                            @isset($actualPrices[$auction->id])
                                                 <td>
-                                                    {{number_format($auction->closing_price ,0 ,"", " ")}}	Kč
+                                                        {{number_format($actualPrices[$auction->id] ,0 ,"", " ")}}	Kč
                                                 </td>
-                                            @endif
+                                            @else    
+                                                <td>-</td>   
+                                            @endisset
+                                        </tr>
+                                        <tr>
+                                            <td colspan="2" class="text-center">
+                                                <button  type="button" class="btn dont-propagate btn-success" onclick="openModal('{{ $auction->id }}',
+                                                '{{ route('image.displayImage',$auction->auctionItem->image) }}', '{{ $auction->auctionItem->item_name }}', `{{ $auction->auctionItem->description }}`,
+                                                '{{ $auction->starting_price }}', '{{ $auction->closing_price }}', '{{ $auction->is_approved }}', '{{ $auction->bid_min }}', '{{ $auction->bid_max }}',
+                                                '{{ $auction->start_time }}', '{{ $auction->time_limit }}', '{{ $auction->is_open  }}', '{{ $auction->is_selling }}')">
+                                                    <div class="d-flex align-content-center">
+                                                        <span class="clickable material-icons-outlined md-24 mr-3" style="height: 24px;" >edit</span>
+                                                        Upravit aukci
+                                                    </div>  
+                                                </button>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
