8be2426095f441ca04a25d08145f05efd6e195ce
Vladislav <sokolovskii.vladislav@gmail.com>
2021-11-29 21:42:38 +0100

    AWS communication is disabled

--- app/Http/Controllers/ImageDisplayController.php
+++ app/Http/Controllers/ImageDisplayController.php
@@ -11,16 +11,17 @@ use Storage;
 class ImageDisplayController extends Controller
 {
     public function displayImage($filename) {
-        if (Storage::disk('local')->has("public/images/" . $filename)) {
-            return  Storage::disk('local')->response("public/images/" . $filename);
-        }
-        if (Storage::disk('s3')->has("public/images/" . $filename)) {
-            $s3_file = Storage::disk('s3')->get("public/images/" . $filename);
-            $local_storage = Storage::disk('local');
-            $local_storage->put("public/images/" . $filename, $s3_file);
-            return  Storage::disk('s3')->response("public/images/" . $filename);
-        } else {
-            return  Storage::disk('s3')->response("public/images/empty.png");
-        }
+        return  Storage::disk('local')->response("public/images/empty.png");
+        // if (Storage::disk('local')->has("public/images/" . $filename)) {
+        //     return  Storage::disk('local')->response("public/images/" . $filename);
+        // }
+        // if (Storage::disk('s3')->has("public/images/" . $filename)) {
+        //     $s3_file = Storage::disk('s3')->get("public/images/" . $filename);
+        //     $local_storage = Storage::disk('local');
+        //     $local_storage->put("public/images/" . $filename, $s3_file);
+        //     return  Storage::disk('s3')->response("public/images/" . $filename);
+        // } else {
+        //     return  Storage::disk('s3')->response("public/images/empty.png");
+        // }
     }
 }
